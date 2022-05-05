6f582746631699132068a24acac6155c183efecd
mouliswaran <104911211+mouliswaran@users.noreply.github.com>
2022-05-05 12:38:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "eu-west-1"
   }
 }
