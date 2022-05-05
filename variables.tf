d03e87755df7bde65e3128d1ea2b135eb8431198
mouliswaran <104911211+mouliswaran@users.noreply.github.com>
2022-05-05 12:45:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "demo" 
+    default     = "demo_1" 
 }
 
 variable "tag_name" { 
